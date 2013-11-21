b65a3e7c6516faf7643dfc1797e72d38fc0f97ab
Ekow Abaka Ainooson <jainooson@gmail.com>
2013-11-21 20:35:32 +0000

    First test suite on the way

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -1,1 +1,13 @@
+#include "../src/Lexer.h"
+#include "gtest/gtest.h"
 
+TEST(LexerTest, Tokens) {
+    Lexer * lexer = new Lexer("dims.bas");
+    EXPECT_EQ(DIM, lexer->getNextToken());
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
