803eb0608fbc7fc770c8d98b0bdeda80bb28a455
James Ainooson <jainooson@gmail.com>
2017-03-26 18:12:30 -0500

    Added a node returned for assignments

--- src/ast/nodes/AssignmentNode.vb
+++ src/ast/nodes/AssignmentNode.vb
@@ -5,6 +5,7 @@ Option Strict On
 Public Class AssignmentNode
     Implements AstNode
 
-    
+    Public identifier As string
+    Public expression As ExpressionNode
 
 End Class
--- src/ast/parsers/AssignmentParser.vb
+++ src/ast/parsers/AssignmentParser.vb
@@ -8,16 +8,17 @@ Imports System
 Public Class AssignmentParser
     Inherits NodeParser
 
-    private identifier as string
-    private expression as ExpressionNode
-
     public overrides function parse() As AstNode
         if parser.lookAhead <> Token.IDENTIFIER then return Nothing
-        identifier = parser.getLexer().tokenString
+
+        Dim node As AssignmentNode = New AssignmentNode
+        node.identifier = parser.getLexer().tokenString
         parser.getNextToken()
         parser.match(Token.EQUALS_OPERATOR)
         parser.getNextToken()
-        expression = CType(parser.getExpressionParser().parse(), ExpressionNode)
+        node.expression = CType(parser.getExpressionParser().parse(), ExpressionNode)
+
+        Return node
     end function
 End Class
 
